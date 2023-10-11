92d8fd49c203b6c2f9963a0f17e7ca60a074fa37
Olexandr <1san4ik@gmail.com>
2023-10-12 00:05:15 +0300

    Homework 43

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [tasks, setTask] = useState([]);
+
+  const [input, setInput] = useState("");
+
+  function todoComplete(id) {
+    setTask(
+      tasks.filter((task) => {
+        if (task.id === id) {
+          task.completed = !task.completed;
+        }
+        return task;
+      })
+    );
+  }
+
+  function todoDelete(id) {
+    setTask(tasks.filter((task) => task.id !== id));
+  }
+
+  function addTodoTask(e) {
+    if (
+      e.code === "Enter" ||
+      (e.code === "NumpadEnter" && input.trim() !== "")
+    ) {
+      setTask(tasks.concat([{ id: Date.now(), title: input }]));
+      setInput("");
+    }
+  }
+
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>Todo List</h1>
+      <input
+        value={input}
+        type="text"
+        className="inputText"
+        onKeyUpCapture={(e) => addTodoTask(e)}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {tasks &&
+        tasks.map((task) => {
+          return (
+            <div className="todoList" key={task.id}>
+              <input
+                type="checkbox"
+                onClick={() => todoComplete(task.id)}
+                checked={task.completed}
+              />
+              <div
+                className="taskTitle"
+                style={{
+                  textDecoration: task.completed ? "line-through" : null,
+                }}
+              >
+                {task.title}
+              </div>
+              <div className="close" onClick={() => todoDelete(task.id)}>
+                &#x2716;
+              </div>
+            </div>
+          );
+        })}
+    </div>
   );
 }
-
-export default App;
